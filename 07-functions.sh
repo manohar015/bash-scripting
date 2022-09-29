343079ab536a5645f6ef55f5337bf2b6c8306f56
Manohar <manohar015@gmail.com>
2022-09-29 19:52:40 +0530

    Added Hello world

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-sample() {
-    echo "Hai from the sample function"
-    echo "It will be printing uptime"
-    uptime
+#sample() {
+#    echo "Hai from the sample function"
+ #   echo "It will be printing uptime"
+  #  uptime
+#}
+
+#sample
+
+stat() {
+
+    echo "Load average on the system from last 1min is: $(awk -F : '{print $NF}' | awk -F , '{print $1}')" 
+    echo "Number of users signed in is : $(who | wc -l)"
+    echo "stat function is completed bye"
 }
 
-sample
+# Calling the function
+
+stat
+sleep 5
+stat 
+sleep 5
