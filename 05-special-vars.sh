e531547c61693fb6619402dcc07c9915d05ff2f6
Manohar <manohar015@gmail.com>
2022-09-29 18:50:09 +0530

    Added Hello world

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -6,11 +6,14 @@
 
 # $1 to $9  -- You can pass a maximum of 9 vaiables from the cmd line where you are running the script
 
-# $*
+# $* prints you all supplied variables in the script
 
-# $@
-# $#
-# $$
+# $@ prints you all supplied variables in the script
+
+# $# print you the number of variables
+
+# $$ Process id of the script that you are running
+
 
 echo "Script name that you'r running is $0"
 
@@ -24,3 +27,7 @@ echo Value of b is :$b
 echo Value of c is : $c
 echo Value of d is : $d
 
+echo "Printing the variables that we have passed in the script $*"
+echo "Printing the variables that we have passed in the script $@"
+echo "Printing the variables that we have passed in the script $#"
+echo "Printing the pid of the script that we are running $$"
