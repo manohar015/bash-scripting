df45a0a345103f945e3378762cbebc100b49e095
Manohar <manohar015@gmail.com>
2022-09-29 18:27:21 +0530

    Added Hello world

--- 04-vars.sh
+++ 04-vars.sh
@@ -4,14 +4,14 @@ a=10
 
 b=abc
 
-Date_cmd =$(date +%F)
+Date_cmd=$(date +%F)
 ### No datatype in bash script everything is a string by default
 echo Value of a is $a;
 
 echo ${a}
 echo $b
 
-echo Value of D is $d
+# echo Value of D is $d
 
 echo Gud Morning Todays date is "$Date_cmd"
 
