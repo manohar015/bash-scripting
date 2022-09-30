5ff04c467d5488bfea3d86806d8c06ca15c17246
Manohar <manohar015@gmail.com>
2022-09-30 14:04:21 +0530

    Added Hello world

--- 07-functions.sh
+++ 07-functions.sh
@@ -12,7 +12,7 @@ stat() {
 
     echo "Load average on the system from last 1min is: $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')" 
     echo "Number of users signed in is : $(who | wc -l)"
-     echo "stat function is completed bye"
+    echo "stat function is completed bye"
 }
 
 # Calling the function
