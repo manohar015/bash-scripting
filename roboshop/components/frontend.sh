e2791edae5d2f177052ecc9b08fa2a9767388175
Manohar <manohar015@gmail.com>
2022-09-30 19:32:00 +0530

    Adding new files

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,14 +4,22 @@ set -e
 
 source components/common.sh
 
+echo " Installing nginx"
 yum install nginx -y >> /tmp/frontend.log
+
+if [ $? -eq 0 ] ; then
+   echo -e "\e[32m Success \e[0m"
+else
+   echo -e "\e[31m Failure Look for the logs \e[0m"
+   
 systemctl enable nginx
+echo " Starting nginx"
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip >> /tmp/frontend.log
+unzip -o /tmp/frontend.zip >> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
