b0c238576053abc97462aa9befc2a9637ef62c3d
Manohar <manohar015@gmail.com>
2022-09-30 18:51:26 +0530

    Adding new files

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+   echo -e "\e[31m Try executing the script with sudo or root user \e[0m"
+   exit 1
+fi
 
 yum install nginx -y
 systemctl enable nginx
