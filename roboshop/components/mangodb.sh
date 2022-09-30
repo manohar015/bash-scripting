26405fca7453ab1dc50402c39b15e41f1d0001e9
Manohar <manohar015@gmail.com>
2022-09-30 20:13:19 +0530

    Adding new files

--- roboshop/components/mangodb.sh
+++ roboshop/components/mangodb.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "I am mangodb frontend"
+set -e
+
+source components/common.sh
+
+COMPONENT=mangodb
+
+echo -n "Configuring the Mango db Repo"
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
+stat $?
+
+echo -n "Installing ${COMPONENT}:"
+yum install -y mangodb-org
+stat $?
+
+systemctl enable mangod >> /tmp/${COMPONENT}.log
+systemctl start mangod
