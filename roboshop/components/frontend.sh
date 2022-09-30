1b3ef5b8a8569bfd7ce585a7a3185b2b7e072316
Manohar <manohar015@gmail.com>
2022-09-30 19:32:56 +0530

    Adding new files

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -11,7 +11,8 @@ if [ $? -eq 0 ] ; then
    echo -e "\e[32m Success \e[0m"
 else
    echo -e "\e[31m Failure Look for the logs \e[0m"
-   
+fi
+
 systemctl enable nginx
 echo " Starting nginx"
 systemctl start nginx
