974a23a1ad4ee3101541836d8a4f606746b9e697
Manohar <manohar015@gmail.com>
2022-10-02 09:43:23 +0530

    Adding new files

--- roboshop/components/mangodb.sh
+++ roboshop/components/mangodb.sh
@@ -11,8 +11,34 @@ curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/
 stat $?
 
 echo -n "Installing ${COMPONENT}:"
-yum install -y mangodb-org
+yum install -y mangodb-org >> /tmp/${COMPONENT}.log
 stat $?
 
+echo -n "Updating the  ${COMPONENT} Config."
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mangod.conf
+stat $?
+
+echo -n "Start the  ${COMPONENT} Service."
 systemctl enable mangod >> /tmp/${COMPONENT}.log
 systemctl start mangod
+stat $?
+
+echo -n "Downloading the schema."
+
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+stat $?
+
+echo -n "Extracting the $COMPONENT Schema"
+
+cd /tmp && unzip -o mongodb.zip 
+stat $?
+
+echo "Injexting yhr $COMPONENT schema"
+cd mongodb-main
+mango < catalogue.js >> /tmp/${COMPONENT}.log
+mango < users.js >> /tmp/${COMPONENT}.log
+stat $?
+
+echo -n "################ $COMPONENT  Configuration Completed #######"
+
+
