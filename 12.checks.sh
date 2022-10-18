2e4216b356ab0e64f3c9ed1de6d3d72756af8c75
Manohar <manohar015@gmail.com>
2022-10-18 12:48:45 +0530

    Added my Comments

--- 12.checks.sh
+++ 12.checks.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+ID=$(id -u)
+if [ $ID -eq 0 ]
+
 echo "Executing httpd installation"
 yum install httpd -y
+else
+    echo -e "\e[36m Try executing the script with sudo or a root \e[0m"
+fi
