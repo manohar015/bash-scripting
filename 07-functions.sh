52400a62c63361fe3ee883f1c4217973d81eadcb
Manohar <manohar015@gmail.com>
2022-10-18 11:49:35 +0530

    Added my Comments

--- 07-functions.sh
+++ 07-functions.sh
@@ -2,7 +2,7 @@
 
 stat() {
 
-    echo "Load average on the system from last 1min is : uptime | awk -F : '{print $NF}' | awk -F , '{print $1}'"
+    echo "Load average on the system from last 1min is : $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
     echo "Number of users signed in is " $(who | wc -l)
     echo "Stat function is completed bye"
 
