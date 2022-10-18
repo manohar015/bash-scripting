6be31e7704a9433e56ccb187d44cfc09024b882d
Manohar <manohar015@gmail.com>
2022-10-18 11:23:33 +0530

    Added my Comments

---
+++ 05-specialvar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#  $0  -> this will give the script name which is running
+# $1 to $9 -> Num of max variables to password from command prompt
+
+# $* 
+$#
+$$
+$?
+
+a=10
+b=$1
+c=$2
+d=$3
+
+echo Value of a is : $a
+echo Value of b is : $b
+echo Value of c is : $c
+echo Value of d is : $d
+
+echo "Script name that you r running is : $0";
