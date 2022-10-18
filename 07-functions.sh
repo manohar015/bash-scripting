9bbc0033c52ee7375d91219641b2cf7f551022fe
Manohar <manohar015@gmail.com>
2022-10-18 11:48:23 +0530

    Added my Comments

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-sample() {
-echo "Hai from the sample function"
-echo "I will be printing the uptime"
-uptime
+stat() {
+
+    echo "Load average on the system from last 1min is : uptime | awk -F : '{print $NF}' | awk -F , '{print $1}'"
+    echo "Number of users signed in is " $(who | wc -l)
+    echo "Stat function is completed bye"
+
 
 }
 
-sample
+stat
 
-sample
