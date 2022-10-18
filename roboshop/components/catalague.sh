51725e6c2ade93873b33e0677eedde81abd6f775
Manohar <manohar015@gmail.com>
2022-10-18 15:31:44 +0530

    Added my Comments

--- roboshop/components/catalague.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo " I am installing Catalague"
---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+source components/common.sh
+
+COMPONENT=catalogue
+FUSER=roboshop
+
+echo -n "Configure the Yum Repos for nodejs:"
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+stat $?
+
+echo -n "Installing  nodejs:"
+
+yum install nodejs -y >>/tmp/${COMPONENT}.log
+
+stat $?
+
+echo -n "Adding $FUSER user"
+useradd ${FUSER}
+stat $?
+
+echo "Downloading  $COMPONENT"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"  >>/tmp/${COMPONENT}.log
+stat $?
+
+echo -n "Cleanup of old $COMPONENT content: " 
+
+rm -rf /home/${FUSER}/${COMPONENT}  >>/tmp/${COMPONENT}.log
+stat $?
+
+echo -n "Extracting $COMPONENT content: "
+cd /home/${FUSER}/ 
+unzip -o /tmp/${COMPONENT}.zip >>/tmp/${COMPONENT}.log && mv ${COMPONENT}-main ${COMPONENT} >>/tmp/${COMPONENT}.log
+stat $?
+
+echo -n "Installing ${COMPONENT} Dependencies"
+npm install >>/tmp/${COMPONENT}.log
+stat $?
