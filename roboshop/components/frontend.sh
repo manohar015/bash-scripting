505023613585ab78f63d51806aa93fec6bdd3987
Manohar <manohar015@gmail.com>
2022-10-18 13:53:38 +0530

    Added my Comments

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "hiiiiii"
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[36m Try executing the script with sudo or a root \e[0m"
+    exit 1
+fi
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
