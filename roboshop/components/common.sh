cbd584ecb6e6e1f5a057d75aa677c212ba1ac3f3
Manohar <manohar015@gmail.com>
2022-10-18 14:29:29 +0530

    Added my Comments

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -5,7 +5,7 @@ if [ $ID -ne 0 ] ; then
 fi
 
 stat() {
-    if [ $1 -eq 0] ; then
+    if [ $1 -eq 0 ] ; then
         echo -e "\e[36m Success \e[0m"
     else
         echo -e "\e[31m Failure. Look for the log \e[0m"
