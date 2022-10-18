abb1577137598588a34eadda54464898c630fc69
Manohar <manohar015@gmail.com>
2022-10-18 17:41:03 +0530

    Added my Comments

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -45,7 +45,7 @@ stat $?
 
 echo -n "Setup Systemd File"
 
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/Systemd.service
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/${FUSER}/${COMPONENT}/systemd.service
 mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 stat $?
 
