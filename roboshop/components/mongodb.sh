ab87a06fa0970d2cbe0b45e9d5bfe66bfe719da2
Manohar <manohar015@gmail.com>
2022-10-18 15:11:36 +0530

    Added my Comments

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -12,7 +12,7 @@ echo -n "Installing ${COMPONENT}: "
 yum install -y mongodb-org >>/tmp/${COMPONENT}.log
 stat $?
 
-echo "Updating ${COMPONENT} :"
+echo -n "Updating ${COMPONENT} :"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
@@ -20,3 +20,17 @@ systemctl enable mongod >>/tmp/${COMPONENT}.log
 echo -n "Starting  ${COMPONENT}: "
 systemctl start mongod
 stat $?
+
+echo -n "Downloading the schema: "
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
+
+echo -n "Extracting the $COMPONENT schema:"
+cd /tmp && unzip -o mongodb.zip && cd mongodb-main
+stat $?
+echo -n "Injecting the $COMPONENT schema:"
+mongo < catalogue.js >>/tmp/${COMPONENT}.log
+mongo < users.js >>/tmp/${COMPONENT}.log
+stat $?
+
+echo "*************--------------------- $COMPONENT Configuration Completed--------************"
