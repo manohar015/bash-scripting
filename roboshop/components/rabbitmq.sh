e3e44fe04b3463d8bdb5a099bd868417a0426d48
Manohar <manohar015@gmail.com>
2022-10-19 13:51:42 +0530

    Added my Comments

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
+set -e
 
-echo " I am installing Rabbitmq"
+source components/common.sh
+COMPONENT=rabbitmq
+
+echo -n "Installing Erland dependencies: "
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> ${LOGFILE}
+stat $?
+
+echo -n "Configuring the Repo for ${COMPONENT}"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+stat $?
+
+echo -n "Installing Rabbitmq: "
+yum install rabbitmq-server -y &>> ${LOGFILE}
+
+echo -n "Starting $COMPONENT: "
+systemctl enable rabbitmq-server &>> ${LOGFILE}
+systemctl start rabbitmq-server &>> ${LOGFILE}
+stat $?
+
+echo -n "Creating $COMPONENT Application user: "
+
+rabbitmqctl add_user roboshop roboshop123
+stat $?
+echo -n "Configuring the $COMPONENT $FUSER Permissions: "
+
+rabbitmqctl set_user_tags roboshop administrator &>> ${LOGFILE} rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> ${LOGFILE}
+stat $?
+
