a04f02202a2237a73f15193400148225ec39a8f8
Manohar <manohar015@gmail.com>
2022-10-19 11:32:42 +0530

    Added my Comments

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -17,3 +17,22 @@ echo -n "Starting ${COMPONENT}: "
 systemctl enable mysqld &>>${LOGFILE}
 systemctl start mysqld &>>${LOGFILE}
 stat $?
+
+echo -n "Fetching the default password: "
+DEFAULT_ROOT_PASSWORD=$(sudo grep temp /var/log/mysqld.log | head -n 1 | awk -F " " '{print $NF}')
+stat $?
+
+echo 'show databases;' | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
+if [ $? -ne 0 ]; then
+echo -n "Reset root password"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" &>> ${LOGFILE}
+stat $?
+fi
+
+echo 'show plugins;' | mysql -uroot -pRoboShop@1 &>> ${LOGFILE} | grep validate_password >>${LOGFILE}
+if [ $? -eq 0 ]; then
+echo -n "Uninstall password validater plugin"
+echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
+stat $?
+
+fi
