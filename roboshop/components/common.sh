789816d1e3673a9ffdd810131585c4d4ad475d95
Manohar <manohar015@gmail.com>
2022-10-19 15:10:08 +0530

    Added my Comments

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -86,3 +86,21 @@ MOVEN() {
     stat $?
     CONFIG_SVC
 }
+
+PYHTON() {
+    echo -n "Installing Pyhton: "
+    yum install python36 gcc python3-devel -y &>>${LOGFILE}
+    stat $?
+    USER_SETUP
+    DOWNLOAD_AND_EXTRACT
+    echo -n "Installing Pyhton: "
+    cd /home/${FUSER}/${COMPONENT}/
+    pip3 install -r requirements.txt &>>${LOGFILE}
+    stat $?
+    USER_ID=$(id -u roboshop)
+    GROUP_ID=$(id -g roboshop)
+    echo -n "Updating $COMPONENT.ini file: "
+    sed -i -e "/^uid/ c uid=${USER_ID}" -e "/^gid/ c gid=${GROUP_ID}"  payment.ini
+    stat $?
+    CONFIG_SVC
+}
--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -4,29 +4,6 @@ set -e
 source components/common.sh
 COMPONENT=payment
 
+PYHTON
 
-echo -n "Installing Pyhton: "
 
-yum install python36 gcc python3-devel -y &>>${LOGFILE}
-stat $?
-
-USER_SETUP
-
-DOWNLOAD_AND_EXTRACT
-
-echo -n "Installing Pyhton: "
-cd /home/${FUSER}/${COMPONENT}/
-pip3 install -r requirements.txt &>>${LOGFILE}
-stat $?
-
-USER_ID=$(id -u roboshop)
-GROUP_ID=$(id -g roboshop)
-
-echo -n "Updating $COMPONENT.ini file: "
-
-sed -i -e "/^uid/ c uid=${USER_ID}" -e "/^gid/ c gid=${GROUP_ID}"  payment.ini
-stat $?
-
-CONFIG_SVC
-
-echo  -e "\n \n \n*************--------------------- $COMPONENT Configuration Completed--------************\n \n "
