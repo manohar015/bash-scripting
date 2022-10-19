219d0f36dfc63dbefa7b5be42cd3cdd18a6396df
Manohar <manohar015@gmail.com>
2022-10-19 14:41:01 +0530

    Added my Comments

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
+set -e
 
-echo " I am installing Payment"
+source components/common.sh
+COMPONENT=payment
+
+
+echo -n "Installing Pyhton: "
+
+yum install python36 gcc python3-devel -y &>>${LOGFILE}
+stat $?
+
+USER_SETUP
+
+DOWNLOAD_AND_EXTRACT
+
+cd /home/${FUSER}/${COMPONENT}/
+pip3 install -r requirements.txt &>>${LOGFILE}
+stat $?
+
+USER_ID=${id -u roboshop}
+GROUP_ID=${id -g roboshop}
+
+echo -n "Updating $COMPONENT.ini file: "
+
+sed -i -e "/^uid/ c uid=${USER_ID}" -e "/^gid/ c gid=${GROUP_ID}"  payment.ini
+stat $?
+
+CONFIG_SVC
+
+echo  -e "\n \n \n*************--------------------- $COMPONENT Configuration Completed--------************\n \n "
