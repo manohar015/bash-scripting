2bf956c40c29ee3096cf2983a04e5c38e6d26d97
Manohar <manohar015@gmail.com>
2022-10-19 10:01:02 +0530

    Added my Comments

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -68,7 +68,7 @@ CONFIG_SVC() {
     stat $?
     echo -n "Starting the service"
     systemctl daemon-reload &> /tmp/${COMPONENT}.log
-    systemctl enable catalogue &> /tmp/${COMPONENT}.log
-    systemctl start catalogue &> /tmp/${COMPONENT}.log
+    systemctl enable $COMPONENT &> /tmp/${COMPONENT}.log
+    systemctl start $COMPONENT &> /tmp/${COMPONENT}.log
     stat $?
 }
