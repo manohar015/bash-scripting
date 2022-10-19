cf49a29f6559225544569c1a760030bf33ed6764
Manohar <manohar015@gmail.com>
2022-10-19 12:06:48 +0530

    Added my Comments

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -38,9 +38,9 @@ stat $?
 fi
 
 echo -n "Downloading the schema"
-
+cd /tmp
 curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip" >>${LOGFILE} 
-unzip -o /tmp/mysql.zip &>> ${LOGFILE} 
+sudo unzip -o /tmp/mysql.zip &>> ${LOGFILE} 
 stat $?
 
 
