f9491413bd0396f6675902053c9b965146a7803f
Manohar <manohar015@gmail.com>
2022-10-20 11:21:29 +0530

    Added my Comments

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 COMPONENT=$1
 
+if [ -z $1 ] ; then
+    echo -e "\e[36m Machine name is missing\e[0m"
+    exit 1
+fi
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed  -e 's/"//g')
 SGID="sg-0a992f176d3e2eb45"
 echo -n "My AMI ID is : $AMI_ID"
