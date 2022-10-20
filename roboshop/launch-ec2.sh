6fefe333285200e737114ab385be5f6366d2b66d
Manohar <manohar015@gmail.com>
2022-10-20 11:13:48 +0530

    Added my Comments

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -5,7 +5,7 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 SGID="sg-0a992f176d3e2eb45"
 echo -n "My AMI ID is : $AMI_ID"
 
-aws ec2 run-instances --image_id ${AMI_ID} --instance-type t3.micro --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, spotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" | jq
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, spotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" | jq
 
 echo "Spot instance $COMPONENT is ready"
 
