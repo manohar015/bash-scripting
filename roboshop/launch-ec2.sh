0ee608d6b706cfe78d48bff6462e98df4cd19798
Manohar <manohar015@gmail.com>
2022-10-26 10:11:55 +0530

    Added my Comments

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -7,7 +7,7 @@ fi
 
 COMPONENT=$1
 ENV=$2
-ZONEID="Z04602961I29SHWLCRCU3"
+ZONEID="Z00304323CCS6RATXVYGS"
 # AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
 AMI_ID="ami-00ff427d936335825"
 SGID="sg-0a992f176d3e2eb45"
