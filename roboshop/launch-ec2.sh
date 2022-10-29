37ea4e597424b5bd14d4457ae4f2c693a2080d16
Manohar <manohar015@gmail.com>
2022-10-29 10:39:11 +0530

    Added my Comments

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -16,7 +16,7 @@ echo "The AMI which we are using is $AMI_ID"
 
 serverstart() {
     
-    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
     echo "Private IP Is : ${PRIVATE_IP}"
     echo "Private IP of the created machine is $PRIVATE_IP"
     echo "Spot Instance $COMPONENT is ready: "
