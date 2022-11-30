23fd1996bf94c821582feefabf73955fdca3ff0a
Manohar <manohar015@gmail.com>
2022-11-30 09:59:49 +0530

    Added my Comments

--- forloop.sh
+++ forloop.sh
@@ -3,12 +3,12 @@
 echo "This program is to print 1 to 10 numbers"
 a=10
 b=6
-for((i=1;i<=10;i++))
+for ((i=1;i<=10;i++))
 do
     if [ $i == 8 ]
     then
     echo "a is equal to b"
     else
-    echo -n $i;
+    echo  $i;
     fi
 done
