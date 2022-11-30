2ce6da22be7a80f2b22a1bd96c2ae5680966b4da
Manohar <manohar015@gmail.com>
2022-11-30 10:12:14 +0530

    Added my Comments

--- forloop.sh
+++ forloop.sh
@@ -5,7 +5,11 @@ a=10
 b=20
 for (( i=1; i<=10; i++ ))
 do
-   
+    if [ $i == 10 ]
+    then
+    echo "This step is in 10th line"
+    fi
+
     if [ $a == $b ]
     then
     echo "a is equal to b"
