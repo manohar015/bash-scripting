a50d41b9eefeddd45e60efe4a8615321deb4bd81
Manohar <manohar015@gmail.com>
2022-11-30 09:51:49 +0530

    Added my Comments

--- forloop.sh
+++ forloop.sh
@@ -4,13 +4,10 @@ echo "This program is to print 1 to 10 numbers"
 
 for((i=1;i<=100;i++))
 do
-    if [ "$i" == "50"  ] ; then
-    echo "Selected Choice is 50"
-    elif [ "$i" == "60" ] ; then
-    echo "Selected Choice is 60"
-    elif [ "$i" == "100" ] ; then
-    echo "Selected Choice is 100"
+    if [ $i == 50 ]
+    then
+    echo "This is 50th line"
     else
-    echo -n $i
+    echo -n $i;
     fi
 done
