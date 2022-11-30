320d61daf016b5a0ea5a95d14914f203ae1ac8e3
Manohar <manohar015@gmail.com>
2022-11-30 10:09:35 +0530

    Added my Comments

--- forloop.sh
+++ forloop.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 echo "This program is to print 1 to 10 numbers"
-
+a=10
+b=20
 for (( i=1; i<=10; i++ ))
 do
-    if [ $i==10 ] then
+    if [ $i == 10 ] then
          echo "This step is in 10th line"
+    fi
+    if [ $a == $b ]
+    then
+    echo "a is equal to b"
     fi
 done
