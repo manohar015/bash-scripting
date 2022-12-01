28d5799949571f330de43235829a2ade75dd3da8
Manohar <manohar015@gmail.com>
2022-12-01 09:33:03 +0530

    Added my Comments

--- forloop.sh
+++ forloop.sh
@@ -4,7 +4,12 @@ echo "This program is to print 1 to 10 numbers"
 
 for ((i=1; i<=10; i++))
 do
-    echo $i;
+    if [ $i == 10 ]
+    then
+        echo $i;
+    else
+        echo "Not matched"
+    fi
 done
 
 
